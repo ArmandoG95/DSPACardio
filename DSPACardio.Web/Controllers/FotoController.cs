0ff7e36c93b2503d6589cfa8067274512994a717
Armando Gonzalez Hernandez <sixmit@hotmail.com>
2022-10-19 15:39:08 -0500

    Creamos controladores y vistas, modificamos el layout.

---
+++ DSPACardio.Web/Controllers/FotoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DSPACardio.Web.Controllers
+{
+    //un controlador es una clase que es publica y lleva nombre y el apellido controller
+    //
+    public class FotoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DSPACardio.Web/Controllers/HomeController.cs
+++ DSPACardio.Web/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace DSPACardio.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Prueba()
+        {
+            return View();
+        }
     }
 }
