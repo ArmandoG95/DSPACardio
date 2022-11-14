6bd29d8a4dd24122ac6b2cceef6bca665af1c24f
Armando Gonzalez Hernandez <sixmit@hotmail.com>
2022-11-14 13:09:04 -0600

    Modifcamos la clase user y la clase patient, creamos la relacion con patien y user con la propiedad MyUser.

--- DBPacientes_EXO/Data/Entities/Patient.cs
+++ DBPacientes_EXO/Data/Entities/Patient.cs
@@ -2,6 +2,8 @@
 {
     public class Patient : User
     {
+        public User MyUser { get; set; }
+
 
     }
 }
--- DBPacientes_EXO/Data/Entities/User.cs
+++ DBPacientes_EXO/Data/Entities/User.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DBPacientes_EXO.Data.Entities
@@ -6,17 +7,20 @@ namespace DBPacientes_EXO.Data.Entities
     public class User: IdentityUser
     {
         public int ID { get; set; }
+
         [Required (ErrorMessage= "{0} Es obligatorio.")]
         [MaxLength(50, ErrorMessage ="El campo {0} no puede tener mas de {1} caracteres.")]
         [Display(Name ="Nombre")]
         public string FirtsName { get; set; }
         public string LastName { get; set; }
-        public int Age { get; set; }
+        public DateTime birthday { get; set; }
         public string Street { get; set; }
         public string StreetNumber { get; set; }
         public string Neighborhood { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+
+        
 
     }
 }
