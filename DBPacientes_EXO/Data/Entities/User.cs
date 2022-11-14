a6f63083d7458402149bf182d14473c91b0b25c6
Armando Gonzalez Hernandez <sixmit@hotmail.com>
2022-11-14 13:29:49 -0600

    Modificamos la clase user agreamos concatenacion de variables fullName

--- DBPacientes_EXO/Data/Entities/User.cs
+++ DBPacientes_EXO/Data/Entities/User.cs
@@ -9,16 +9,24 @@ namespace DBPacientes_EXO.Data.Entities
         public int ID { get; set; }
 
         [Required (ErrorMessage= "{0} Es obligatorio.")]
-        [MaxLength(50, ErrorMessage ="El campo {0} no puede tener mas de {1} caracteres.")]
+        [MaxLength(50, ErrorMessage ="El Nombre {0} no puede tener mas de {1} caracteres.")]
         [Display(Name ="Nombre")]
         public string FirtsName { get; set; }
+
+        [Required(ErrorMessage = "{0} Es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El Apellido {0} no puede tener mas de {1} caracteres.")]
+        [Display(Name = "Apellido")]
         public string LastName { get; set; }
-        public DateTime birthday { get; set; }
+        public DateTime Birthday { get; set; }
         public string Street { get; set; }
         public string StreetNumber { get; set; }
         public string Neighborhood { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
+        [MaxLength(10, ErrorMessage = "")]
+        [Display(Name = "Numero de Telefono")]
+        public override string PhoneNumber { get; set; }
+        public override string Email { get; set; }
+
+        public string FullName => $"{FirtsName}{LastName}";
 
         
 
